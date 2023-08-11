66bb47833b04624bd971391e35d4e07f8a454a4b
mnelson4 <thespacecamel@hotmail.com>
2023-08-11 07:51:06 -0700

    dont resize images

--- designs/digital_pdf/assets/script.js
+++ designs/digital_pdf/assets/script.js
@@ -5,7 +5,7 @@ jQuery(document).on('pmb_doc_conversion_requested', function(){
     //pmb_replace_internal_links_with_page_refs_and_footnotes('footnote', 'footnote', pmb_design_options['external_footnote_text'], pmb_design_options['internal_footnote_text']);
     new PmbToc();
     jQuery(document).on('pmb_done_processing_videos', function() {
-        pmb_resize_images(400);
+        //pmb_resize_images(400);
         jQuery(document).trigger('pmb_doc_conversion_ready');
     });
     pmb_convert_youtube_videos_to_images();
