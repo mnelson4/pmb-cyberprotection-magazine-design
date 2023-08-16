26f8b31ca3338bd96ec15784948630a74fc458ad
mnelson4 <thespacecamel@hotmail.com>
2023-08-16 08:19:45 -0700

    attempt 1 at footer

---
+++ designs/digital_pdf/templates/project.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @var $pmb_project \PrintMyBlog\orm\entities\Project
+ * @var $pmb_design \PrintMyBlog\orm\entities\Design
+ *
+ */
+
+pmb_include_design_template('partials/html_header');
+?>
+<body class="<?php
+//phpcs:ignore -- get_body_class does the escaping
+echo str_replace('has-sidebar', '', implode(' ', get_body_class('pmb-print-page pmb-pro-print-page pmb-pro-print-pdf')));
+?>">
+<?php do_action('pmb_pro_print_window'); ?>
+<div class="pmb-posts pmb-project-content site">
+<div class="pmbcpm-footer-margin-wrap" id="pmbcpm-footer-margin-wrap">
+    <div id="pmbcpm-footer-margin" class="pmbcpm-footer-margin">
+        <div class="pmbcpm-pagenum-wrapper">
+            <span class="pmb-page-number">3</span>
+        </div>
+        <div class="pmbcpm-publication-data-wrapper" >
+            <span class="pmb-publication-data">
+                <?php echo $pmb_design->getSetting('magazine_name'); ?> <?php echo $pmb_project->getSetting('month');?>.<?php echo $pmb_project->getSetting('year');?>
+            </span>
+        </div>
+    </div>
+</div>
