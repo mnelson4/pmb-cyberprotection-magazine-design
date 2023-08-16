5c07db6c19b878c849bab6336ee94b174b8672ca
mnelson4 <thespacecamel@hotmail.com>
2023-08-16 07:57:45 -0700

    try to update header margin

--- designs/digital_pdf/templates/part.php
+++ designs/digital_pdf/templates/part.php
@@ -4,8 +4,8 @@
 
         <header class="entry-header has-text-align-center">
 
-            <div class="entry-header-inner section-inner medium">
-                <?php esc_attr_e(pmb_the_title());?>
+            <div class="entry-header-inner section-inner medium pmbcpm-part-title-wrapper ">
+                <span class="pmbcpm-part-title pmbcpm-part-underline"><?php esc_attr_e(pmb_get_title());?></span>
             </div><!-- .entry-header-inner -->
         </header><!-- .entry-header -->
 
