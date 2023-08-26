6d88910d3d3b47c7a148ab206ef579968301f438
mnelson4 <thespacecamel@hotmail.com>
2023-08-26 00:44:12 -0700

    trying to remove whitespace from parts moved to header

--- designs/digital_pdf/templates/part.php
+++ designs/digital_pdf/templates/part.php
@@ -1,13 +1,9 @@
 <div <?php pmb_section_wrapper_class();?> <?php pmb_section_wrapper_id();?>>
-
-    <article <?php pmb_section_class(); ?> <?php pmb_section_id(); ?>>
-
+    <article <?php pmb_section_class('pmbcpm-part-title-wrapper'); ?> <?php pmb_section_id(); ?>>
         <header class="entry-header has-text-align-center">
-
-            <div class="entry-header-inner section-inner medium pmbcpm-part-title-wrapper ">
+            <div class="entry-header-inner section-inner medium">
                 <span class="pmbcpm-part-title pmb-title pmbcpm-part-underline"><?php esc_attr_e(pmb_get_title());?></span>
             </div><!-- .entry-header-inner -->
         </header><!-- .entry-header -->
-
     </article>
 <?php // div automatically closed
