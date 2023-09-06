8c4887abdeb6beca505c41de5e9d4843d36493a1
mnelson4 <thespacecamel@hotmail.com>
2023-09-06 14:31:48 -0700

    implemen editorial a bit

--- designs/digital_pdf/templates/editorial.php
+++ designs/digital_pdf/templates/editorial.php
@@ -1,1 +1,2 @@
-<?php
+<?php // end of file. For some reason this comment was needed to prevent a fatal parsing error on dev.printmy.blog
+pmb_include_design_template('partials/article_base', ['show_author' => false]);
