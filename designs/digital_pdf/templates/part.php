1817ae7b8c5b9e6e1b1d0539ebc26a95447e4ae7
mnelson4 <thespacecamel@hotmail.com>
2023-09-06 15:53:21 -0700

    rename part title to top margin title because its more general

--- designs/digital_pdf/templates/part.php
+++ designs/digital_pdf/templates/part.php
@@ -1,8 +1,8 @@
 <div <?php pmb_section_wrapper_class();?> <?php pmb_section_wrapper_id();?>>
-    <article <?php pmb_section_class('pmbcpm-part-title-wrapper'); ?> <?php pmb_section_id(); ?>>
+    <article <?php pmb_section_class('pmbcpm-top-margin-title-wrapper'); ?> <?php pmb_section_id(); ?>>
         <header class="entry-header has-text-align-center">
             <div class="entry-header-inner section-inner medium">
-                <span class="pmbcpm-part-title pmb-title pmbcpm-part-underline"><?php esc_attr_e(pmb_get_title());?></span>
+                <span class="pmbcpm-top-margin-title pmb-title pmbcpm-part-underline"><?php esc_attr_e(pmb_get_title());?></span>
             </div><!-- .entry-header-inner -->
         </header><!-- .entry-header -->
     </article>
