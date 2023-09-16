682fd55cdd938736cb3f3f30d442a5eeddf40c3b
mnelson4 <thespacecamel@hotmail.com>
2023-09-15 21:31:51 -0700

    undo unsuccessful attempt ot remove blank page by putting the page break at the start o fa part and remove it form the first article. it didnt work because there is an implied page break for each article because they are styled differently. instead try to remove that blank line thats appearing, maybe because of whitespace between divs

--- designs/digital_pdf/templates/partials/article_base.php
+++ designs/digital_pdf/templates/partials/article_base.php
@@ -11,8 +11,7 @@ if(! isset($featured_image_class)){
 if(! isset($show_author)){
     $show_author = true;
 }
-?>
-<div <?php pmb_section_wrapper_class($featured_image_class . '-featured-image');?> <?php pmb_section_wrapper_id();?>>
+?><div <?php pmb_section_wrapper_class($featured_image_class . '-featured-image');?> <?php pmb_section_wrapper_id();?>>
     <article <?php pmb_section_class(); ?> <?php pmb_section_id(); ?>>
         <header class="entry-header has-text-align-center">
             <div class="entry-header-inner section-inner medium">
