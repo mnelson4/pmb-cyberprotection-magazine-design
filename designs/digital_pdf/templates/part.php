9c91198896fcc51fe1e7c98c139659905cefbb06
mnelson4 <thespacecamel@hotmail.com>
2023-09-15 20:10:43 -0700

    remove whitespace around part that gets moved into the heading margin

--- designs/digital_pdf/templates/part.php
+++ designs/digital_pdf/templates/part.php
@@ -1,9 +1,7 @@
-<div <?php pmb_section_wrapper_class();?> <?php pmb_section_wrapper_id();?>>
-    <article <?php pmb_section_class('pmbcpm-top-margin-title-wrapper'); ?> <?php pmb_section_id(); ?>>
+<div <?php pmb_section_wrapper_class();?> <?php pmb_section_wrapper_id();?>><article <?php pmb_section_class('pmbcpm-top-margin-title-wrapper'); ?> <?php pmb_section_id(); ?>>
         <header class="entry-header has-text-align-center">
             <div class="entry-header-inner section-inner medium">
                 <span class="pmbcpm-top-margin-title pmb-title pmbcpm-part-underline"><?php esc_attr_e(pmb_get_title());?></span>
             </div><!-- .entry-header-inner -->
         </header><!-- .entry-header -->
-    </article>
-<?php // div automatically closed
+    </article><?php // div automatically closed
